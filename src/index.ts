5eae0ade9fd2059fa1e2e13c4aa640cb5726ab85
nshiab <nael.shiab@gmail.com>
2026-05-05 15:27:14 -0400

    v1.0.10

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
+/**
+ * @module
+ *
+ * The Journalism library (Google services functions)
+ *
+ * To install the library with Deno, use:
+ * ```bash
+ * deno add jsr:@nshiab/journalism-google
+ * ```
+ *
+ * To install the library with Node.js, use:
+ * ```bash
+ * npm i @nshiab/journalism-google
+ * ```
+ *
+ * To import a function, use:
+ * ```ts
+ * import { functionName } from "@nshiab/journalism-google";
+ * ```
+ */
+
 import addSheetRows from "./google/addSheetRows.ts";
 import overwriteSheetData from "./google/overwriteSheetData.ts";
 import getSheetData from "./google/getSheetData.ts";
